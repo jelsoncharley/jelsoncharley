36e2ad3f4c03088f7f57948f028ffc96f46dace7
jelsoncharley <jelsoncharley@yahoo.co.in>
2025-10-25 18:53:33 -0300

    Testing Hash Routing

--- src/main.tsx
+++ src/main.tsx
@@ -3,15 +3,15 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 import { Header } from './shared/header.tsx'
-import { BrowserRouter } from 'react-router'
+import { HashRouter } from 'react-router'
 import { Footer } from './shared/footer.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <Header />
-    <BrowserRouter >
+    <HashRouter >
       <App />
-    </BrowserRouter>
+    </HashRouter>
     <Footer />
   </StrictMode>,
 )
