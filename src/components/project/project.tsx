48fdbda9885d8926041c8985b3107d830c37c777
jelsoncharley <jelsoncharley@yahoo.co.in>
2025-10-27 15:26:23 -0300

    Removed scrollbar

--- src/components/project/project.tsx
+++ src/components/project/project.tsx
@@ -6,7 +6,7 @@ import { ProjectCard } from "./projectCard";
 export function Project() {
     return <>
         <div className="font-sans items-center justify-items-center sm:p-20 h-full">
-            <div className="md:grid md:grid-cols-5 gap-5 overflow-y-scroll">
+            <div className="md:grid md:grid-cols-5 gap-5">
                 {
                     projectData.map((project, idx) => <ProjectCard {...project} key={project.name + idx}></ProjectCard>)
                 }
